fb3945c2546ed7f7765b57681d8bd18bf93ccfc0
Dzmitry Harhun <dimagorga@gmail.com>
2021-06-09 23:45:02 +0300

    добавляет разметку

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import templateMurkup from './templates/template.hbs';
+import menuCards from '../menu.json';
+
+const menuList = document.querySelector('.js-menu');
+menuList.insertAdjacentHTML('beforeend', createMurkup(menuCards));
+
+function createMurkup(menu){
+    return templateMurkup(menu)
+}
